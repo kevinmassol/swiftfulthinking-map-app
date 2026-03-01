ed26c2bc1deac640308da10a97fdc22675dc6c46
Kevin Massol <kevin.massol@icloud.com>
2026-03-01 15:15:02 +0100

    Initial Commit

---
+++ SwiftfulThinkingMapApp/SwiftfulThinkingMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingMapAppApp.swift
+//  SwiftfulThinkingMapApp
+//
+//  Created by KEVIN on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
