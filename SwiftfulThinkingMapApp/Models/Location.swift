8685ac2c75b1e964aa6f8b857daf9e8cff730bd3
Kevin Massol <kevin.massol@icloud.com>
2026-03-02 06:33:42 +0100

    🚀 feat: add location file and create the model

---
+++ SwiftfulThinkingMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulThinkingMapApp
+//
+//  Created by KEVIN on 02/03/2026.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
