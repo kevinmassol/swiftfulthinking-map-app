aaf07bcc94d2b8679fade8709dcaaa22384da766
Kevin Massol <kevin.massol@icloud.com>
2026-03-02 07:11:26 +0100

    🚀 feat: add Map with MapKit

--- SwiftfulThinkingMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftfulThinkingMapApp/ViewModels/LocationsViewModel.swift
@@ -5,15 +5,39 @@
 //  Created by KEVIN on 02/03/2026.
 //
 
+import SwiftUI
 import Foundation
 internal import Combine
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     
+    //All loaded locations
     @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan
+            )
+        }
     }
 }
