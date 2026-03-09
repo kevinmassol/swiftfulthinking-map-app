ef68b0a514396350e95124bba799456468e1e052
Kevin Massol <kevin.massol@icloud.com>
2026-03-09 07:01:03 +0100

    🚀 feat: add List with animation for the menu

--- SwiftfulThinkingMapApp/Views/LocationsView.swift
+++ SwiftfulThinkingMapApp/Views/LocationsView.swift
@@ -10,13 +10,20 @@ internal import Combine
 import MapKit
 
 struct LocationsView: View {
-        
+    
     @EnvironmentObject private var vm: LocationsViewModel
-
+    
     var body: some View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+            }
         }
     }
 }
@@ -24,4 +31,34 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Button(action: vm.toggleLocationsList) {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundStyle(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundStyle(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
+                    }
+            }
+            
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
 }
