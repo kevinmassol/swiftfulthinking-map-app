0f8a82b6a021eeb9df80ab4790a1e1005afc3192
Kevin Massol <kevin.massol@icloud.com>
2026-03-12 17:10:15 +0100

    🚀 feat: location preview cards with asymetric transitions

--- SwiftfulThinkingMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftfulThinkingMapApp/ViewModels/LocationsViewModel.swift
@@ -57,4 +57,28 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = false
         }
     }
+    
+    func nextButtonPressed() {
+        // Get the current index
+        
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+            return print("No current location found")
+        }
+        
+        // Check if the nextIndex is valid
+        
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            // Next index is NOT valid
+            // Restart from 0
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // Next index IS valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
 }
+
