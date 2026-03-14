230ec887626401d30c092974b605dd0c266fada4
Kevin Massol <kevin.massol@icloud.com>
2026-03-14 11:55:43 +0100

    🚀 feat: update SwiftUI for iPad, add launch screen

--- SwiftfulThinkingMapApp/Views/LocationsView.swift
+++ SwiftfulThinkingMapApp/Views/LocationsView.swift
@@ -12,12 +12,15 @@ import MapKit
 struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
     
     var body: some View {
         ZStack {
             mapLayer
             VStack(spacing: 0) {
                 header
+                    .padding()
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
                 locationsPreviewStack
             }
@@ -86,6 +89,8 @@ extension LocationsView {
                     LocationPreviewView(location: location)
                         .shadow(color: Color.black.opacity(0.3), radius: 20)
                         .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
                         .transition(.asymmetric(
                             insertion: .move(edge: .trailing),
                             removal: .move(edge: .leading)))
